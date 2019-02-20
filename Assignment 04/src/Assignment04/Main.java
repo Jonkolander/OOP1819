273f871e69d07221bdc7a840e51572e4816a527b
Hollander, D. den (Dennis) <d.denhollander@student.ru.nl>
2019-02-20 21:58:44 +0100

    A4 | Built a template for the 4th assignment

---
+++ Assignment 04/src/Assignment04/Main.java
@@ -0,0 +1,14 @@
+package Assignment04;
+
+/**
+ * @author Dennis den Hollander (s4776658)
+ * @author Tom Kamp (s4760921)
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("Initializing ...");
+        System.out.println("Initialization done!");
+    }
+    
+}
