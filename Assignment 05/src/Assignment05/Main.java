4609e8771f329a5c9dec3dfe7d4b3b279f0f789c
Hollander, D. den (Dennis) <d.denhollander@student.ru.nl>
2019-02-26 22:35:29 +0100

    A5 | Made a template for assignment 5 and removed some folders in A4

---
+++ Assignment 05/src/Assignment05/Main.java
@@ -0,0 +1,13 @@
+package Assignment05;
+
+/**
+ * @author Dennis den Hollander (s4776658)
+ * @author Tom Kamp (s4760921)
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("Test");
+    }
+    
+}
