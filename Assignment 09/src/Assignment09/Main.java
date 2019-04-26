2fb0a53acebd5d871fc0718b3425b33a356ccb2f
Hollander, D. den (Dennis) <d.denhollander@student.ru.nl>
2019-04-26 19:59:02 +0200

    A9 | Small changes

--- Assignment 09/src/Assignment09/Main.java
+++ Assignment 09/src/Assignment09/Main.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Assignment09;
 
-import java.text.DecimalFormat;
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
@@ -22,21 +16,11 @@ import javafx.util.converter.NumberStringConverter;
 
 /**
  *
- * @author Dennis den Hollander (s4776658)
+ * @author Dennis den Hollander | s4776658
+ * @author Tom Kamp             | s4760921
  */
-public class Assignment09 extends Application {
-  
-  private IntegerProperty sum = new SimpleIntegerProperty();
-  private final IntegerProperty value1 = new SimpleIntegerProperty(1);
-  private final IntegerProperty value2 = new SimpleIntegerProperty(1);
-  private final IntegerProperty value3 = new SimpleIntegerProperty(1);
-  private final IntegerProperty value4 = new SimpleIntegerProperty(1);
-
-  private final DoubleProperty outval1 = new SimpleDoubleProperty();
-  private final DoubleProperty outval2 = new SimpleDoubleProperty();
-  private final DoubleProperty outval3 = new SimpleDoubleProperty();
-  private final DoubleProperty outval4 = new SimpleDoubleProperty();
-
+public class Main extends Application {
+ 
   @Override
   public void start(Stage primaryStage) {
     GridPane root = new GridPane();
@@ -48,7 +32,7 @@ public class Assignment09 extends Application {
     textField1.textProperty().addListener((
       ObservableValue<? extends String> observable, 
       String oldValue, String newValue) -> {
-        if (!newValue.matches("[1 -9]\\d{0,3}")) {
+        if (!newValue.matches("[1-9]\\d{0,3}")) {
           textField1.setText(oldValue);
         }
     });
@@ -57,8 +41,8 @@ public class Assignment09 extends Application {
     textField2.textProperty().addListener((
       ObservableValue<? extends String> observable, 
       String oldValue, String newValue) -> {
-        if (!newValue.matches("[1 -9]\\d{0,3}")) {
-          textField1.setText(oldValue);
+        if (!newValue.matches("[1-9]\\d{0,3}")) {
+          textField2.setText(oldValue);
         }
     });
     
@@ -66,8 +50,8 @@ public class Assignment09 extends Application {
     textField3.textProperty().addListener((
       ObservableValue<? extends String> observable, 
       String oldValue, String newValue) -> {
-        if (!newValue.matches("[1 -9]\\d{0,3}")) {
-          textField1.setText(oldValue);
+        if (!newValue.matches("[1-9]\\d{0,3}")) {
+          textField3.setText(oldValue);
         }
     });
     
@@ -75,57 +59,59 @@ public class Assignment09 extends Application {
     textField4.textProperty().addListener((
       ObservableValue<? extends String> observable, 
       String oldValue, String newValue) -> {
-        if (!newValue.matches("[1 -9]\\d{0,3}")) {
-          textField1.setText(oldValue);
+        if (!newValue.matches("[1-9]\\d{0,3}")) {
+          textField4.setText(oldValue);
         }
     });
-
-    Label A = new Label();
-    Label B = new Label();
-    Label C = new Label();
-    Label D = new Label();
-
-    // Add a fixed number of TextFields
+    
+    IntegerProperty sum = new SimpleIntegerProperty(); 
+    IntegerProperty value1 = new SimpleIntegerProperty(10);
+    IntegerProperty value2 = new SimpleIntegerProperty(10);
+    IntegerProperty value3 = new SimpleIntegerProperty(10);
+    IntegerProperty value4 = new SimpleIntegerProperty(10);
+    
+    DoubleProperty outval1 = new SimpleDoubleProperty();
+    DoubleProperty outval2 = new SimpleDoubleProperty();
+    DoubleProperty outval3 = new SimpleDoubleProperty();
+    DoubleProperty outval4 = new SimpleDoubleProperty();
+  
+    Label output1 = new Label();
+    Label output2 = new Label();
+    Label output3 = new Label();
+    Label output4 = new Label();
+    
     textField1.textProperty().bindBidirectional(value1, new NumberStringConverter());
-    IntegerProperty tempsum1 = new SimpleIntegerProperty();
-    tempsum1.bind(value1);
-
     textField2.textProperty().bindBidirectional(value2, new NumberStringConverter());
-    IntegerProperty tempsum2 = new SimpleIntegerProperty();
-    tempsum2.bind(tempsum1.add(value2));
-
     textField3.textProperty().bindBidirectional(value3, new NumberStringConverter());
-    IntegerProperty tempsum3 = new SimpleIntegerProperty();
-    tempsum3.bind(tempsum2.add(value3));
-
     textField4.textProperty().bindBidirectional(value4, new NumberStringConverter());
-    IntegerProperty tempsum4 = new SimpleIntegerProperty();
-    tempsum4.bind(tempsum3.add(value4));
 
-    sum = new SimpleIntegerProperty(75);
-
+    IntegerProperty partial_sum1 = new SimpleIntegerProperty(); 
+    partial_sum1.bind(value2.add(value1));
+    IntegerProperty partial_sum2 = new SimpleIntegerProperty(); 
+    partial_sum2.bind(partial_sum1.add(value3));
+    
+    sum.bind(partial_sum2.add(value4));
+    
     outval1.bind(value1.add(0f).divide(sum));
     outval2.bind(value2.add(0f).divide(sum));
     outval3.bind(value3.add(0f).divide(sum));
     outval4.bind(value4.add(0f).divide(sum));
     
-    DecimalFormat df = new DecimalFormat("#.0000");
+    output1.textProperty().bind(outval1.asString("%.4f"));
+    output2.textProperty().bind(outval2.asString("%.4f"));
+    output3.textProperty().bind(outval3.asString("%.4f"));
+    output4.textProperty().bind(outval4.asString("%.4f"));
     
-    A.textProperty().bind(outval1.asString());
-    B.textProperty().bind(outval2.asString());
-    C.textProperty().bind(outval3.asString());
-    D.textProperty().bind(outval4.asString());
-
     root.add(textField1, 0, 0);
     root.add(textField2, 0, 1);
     root.add(textField3, 0, 2);
     root.add(textField4, 0, 3);
 
-    root.add(A, 1, 0);
-    root.add(B, 1, 1);
-    root.add(C, 1, 2);
-    root.add(D, 1, 3);
-
+    root.add(output1, 1, 0);
+    root.add(output2, 1, 1);
+    root.add(output3, 1, 2);
+    root.add(output4, 1, 3);
+    
     Scene scene = new Scene(root, 300, 250);
 
     primaryStage.setTitle("Pie Chart Generator");
